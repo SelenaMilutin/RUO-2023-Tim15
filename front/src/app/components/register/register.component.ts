80c208396f57969f2270aa4c4fb156058741984e
SelenaMilutin <selena.milutin@gmail.com>
2023-04-28 22:54:05 +0200

    Add models and AWS configuration

--- front/src/app/components/register/register.component.ts
+++ front/src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
+/// <reference types="aws-sdk" />
+import { registerLocaleData } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import * as AWS from 'aws-sdk'
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -10,7 +12,7 @@ export class RegisterComponent implements OnInit {
 
   constructor() { }
 
-  registerAccountForm = new FormGroup({
+  registerAccountForm: FormGroup = new FormGroup({
     name: new FormControl('', Validators.required),
     surname: new FormControl('', Validators.required),
     birthday: new FormControl('', Validators.required),
@@ -24,6 +26,65 @@ export class RegisterComponent implements OnInit {
   }
 
   registerAccount(): void {
-    
+    if (!this.registerAccountForm.valid){
+      alert("Podaci")
+      return
+    }
+    this.loadtobase()
   }
+
+  loadtobase(): void {
+    // Load the AWS SDK for Node.js
+    // Set the region 
+    AWS.config.update({region: 'Frankfurt',
+                      apiVersion: "2010-12-01",
+                      accessKeyId: "process.env.AWS_SECRET_KEY"});
+                      // accessSecretKey: "process.env.AWS_SECRET_KEY"});
+  
+    // Create the DynamoDB service object
+    var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+  
+    var params = {
+      TableName: 'users_the_second_great_table',
+      Item: {
+        "username": {
+         "S": this.registerAccountForm.value.username
+        },
+        "galeryName": {
+         "S": "root"
+        },
+        "document": {
+         "L": [
+         ]
+        },
+        "name": {
+         "S": this.registerAccountForm.value.name
+        },
+        "lastname": {
+         "S": this.registerAccountForm.value.lastname
+        },
+        "birthday": {
+         "S": this.registerAccountForm.value.birthday
+        },
+        "email": {
+         "S": this.registerAccountForm.value.gmail
+        },
+        "password": {
+         "S": this.registerAccountForm.value.password
+        }
+       }
+    };
+  
+    // Call DynamoDB to add the item to the table
+    ddb.putItem(params, function(err: any, data: any) {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        console.log("Success", data);
+      }
+    });
+  
+  }
+  
 }
+

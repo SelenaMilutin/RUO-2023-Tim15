edba215951700638936b4d5b27a55ea3033448e2
unknown <spremic.katarina@gmail.com>
2023-06-27 21:44:02 +0200

    Move upload lambdas to my-new-service

--- front/my-new-service/gallery/uploadDDB.js
+++ front/my-new-service/gallery/uploadDDB.js
@@ -1,23 +1,28 @@
 const AWS = require("aws-sdk");
 const docClient = new AWS.DynamoDB.DocumentClient();
+const createResponse = require('../utility/utils.js').createResponse;
 
 module.exports.upload = async (event, context) => {
     console.log("recieved event", JSON.stringify(event, null, 2))
     console.log("recieved context", JSON.stringify(context, null, 2))
 
-    var file = event
+    // Previous step has failed
+    if (event.hasOwnProperty('statusCode')) return event;
+
+    var file = event['body'];
+    const now = new Date();
 
     var object = {
-    TableName: 'serverlessGallery',
+    TableName: 'gst-serverlessGallery',
     Item: {
       "fileName": file.fileName,
       "fileType": file.fileType,
       "fileSize": file.fileSize,
-      "dateCreated": file.dateCreated,
-      "dateModified": file.dateModified,
+      "dateCreated": now.toISOString(),
+      "dateModified": now.toISOString(),
       "description": file.description,
       "tags": file.tags,
-      "owner": file.owner,
+      "fileOwner": file.owner,
       "hasAccess": file.hasAccess,
       "s3Name": file.s3Name,
       "albumName": file.albumName
@@ -27,9 +32,10 @@ module.exports.upload = async (event, context) => {
 
   try {
     await docClient.put(object).promise();
-    return { body: 'Successfully created item!' }
-  } catch (err) {
-    return { error: err }
+    return createResponse(200, 'Successfully created item!');
+  } catch (error) {
+    console.log(error)
+    return createResponse(400, 'Error');
   }
 
 }
